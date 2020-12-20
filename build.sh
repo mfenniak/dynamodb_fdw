26ffb0a7932218092fb078c10716d0b4137727fb
Mathieu Fenniak <biziqe@mathieu.fenniak.net>
2020-12-20 13:54:33 -0700

    Fix docker push

--- build.sh
+++ build.sh
@@ -11,9 +11,8 @@ docker build -f Dockerfile -t mfenniak/dynamodb_fdw:${CODEBUILD_BUILD_NUMBER} .
 if [ "$CODEBUILD_WEBHOOK_TRIGGER" == "branch/main" ];
 then
     docker tag mfenniak/dynamodb_fdw:${CODEBUILD_BUILD_NUMBER} mfenniak/dynamodb_fdw:latest
-    docker push \
-        mfenniak/dynamodb_fdw:${CODEBUILD_BUILD_NUMBER} \
-        mfenniak/dynamodb_fdw:latest
+    docker push mfenniak/dynamodb_fdw:${CODEBUILD_BUILD_NUMBER}
+    docker push mfenniak/dynamodb_fdw:latest
 
     curl -f -v -X POST \
         -H "Content-Type: application/json" \
