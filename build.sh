548b35ec8f8c9c64f569464cdef6a0072c3f67f7
Mathieu Fenniak <mathieu@fenniak.net>
2024-05-04 19:31:47 -0600

    Rough rework of codebuild for nix-based build

--- build.sh
+++ build.sh
@@ -6,21 +6,43 @@ echo "CODEBUILD_BUILD_NUMBER = $CODEBUILD_BUILD_NUMBER"
 echo "CODEBUILD_WEBHOOK_TRIGGER = $CODEBUILD_WEBHOOK_TRIGGER"
 echo "CODEBUILD_RESOLVED_SOURCE_VERSION = $CODEBUILD_RESOLVED_SOURCE_VERSION"
 
-# Warning: cant make the CodeBuild logs public, because that will expose the environment variables in logs...
-echo $DOCKER_PASS | docker login --username mfenniak --password-stdin
+sed -i"" -e "s/9.9/$CODEBUILD_BUILD_NUMBER/" ./flake.nix
 
-docker build -f Dockerfile -t mfenniak/dynamodb_fdw:${CODEBUILD_BUILD_NUMBER} .
+###########################################################
+# Build container
 
-if [ "$CODEBUILD_WEBHOOK_TRIGGER" == "branch/main" ];
-then
-    docker tag mfenniak/dynamodb_fdw:${CODEBUILD_BUILD_NUMBER} mfenniak/dynamodb_fdw:latest
-    docker push mfenniak/dynamodb_fdw:${CODEBUILD_BUILD_NUMBER}
-    docker push mfenniak/dynamodb_fdw:latest
+nix build --print-build-logs ".#docker"
 
-    # Warning: cant make the CodeBuild logs public, because that will expose the environment variables in logs...
-    curl -f -v -X POST \
-        -H "Content-Type: application/json" \
-        --data "{\"tag_name\":\"$CODEBUILD_BUILD_NUMBER\",\"name\":\"$CODEBUILD_BUILD_NUMBER\",\"target_commitish\":\"$CODEBUILD_RESOLVED_SOURCE_VERSION\" }" \
-        -u $GITHUB_USER:$GITHUB_TOKEN \
-        https://api.github.com/repos/mfenniak/dynamodb_fdw/releases
-fi
+###########################################################
+# Perform publish of docker image
+
+export vtag=ghcr.io/mfenniak/dynamodb_fdw:$CODEBUILD_BUILD_NUMBER
+export ltag=ghcr.io/mfenniak/dynamodb_fdw:latest
+
+# `result` should be the built container from 04-build.sh
+docker load < result
+
+# if [ "$CODEBUILD_WEBHOOK_TRIGGER" == "branch/main" ];
+# then
+
+# ensure latest & versioned tag are applied to the image
+docker tag $vtag $ltag
+docker push $vtag
+docker push $ltag
+
+# fi
+
+###########################################################
+# Create GitHub release tag
+
+# if [ "$CODEBUILD_WEBHOOK_TRIGGER" == "branch/main" ];
+# then
+
+set +x # stop cmd logging for auth security
+curl -v -X POST -H "Content-Type:application/json" \
+    --data "{\"tag_name\":\"$CODEBUILD_BUILD_NUMBER\",\"name\":\"$CODEBUILD_BUILD_NUMBER\",\"target_commitish\":\"$CODEBUILD_RESOLVED_SOURCE_VERSION\" }" \
+    -u $GITHUB_USER:$GITHUB_TOKEN \
+    https://api.github.com/repos/mfenniak/dynamodb_fdw/releases
+set -x
+
+# fi
