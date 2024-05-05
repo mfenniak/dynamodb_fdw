2316c0da830bd0f4399c468a224be7813e44e832
Mathieu Fenniak <mathieu@fenniak.net>
2024-05-04 19:54:38 -0600

    remove extrausers opt

--- flake.nix
+++ flake.nix
@@ -139,8 +139,8 @@
             #!${pkgs.runtimeShell}
             set -e
             ${pkgs.dockerTools.shadowSetup}
-            groupadd --system -g 999 postgres --extrausers
-            useradd --system --no-create-home -u 999 -g 999 postgres --extrausers
+            groupadd --system -g 999 postgres
+            useradd --system --no-create-home -u 999 -g 999 postgres
             chown -R postgres:postgres data
           '';
           enableFakechroot = true;
